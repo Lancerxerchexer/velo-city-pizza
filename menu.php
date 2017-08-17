9cf266e6c8b45e46c605c53ca7462ec81555362c
Lancerxerchexer <lancecorner@gmail.com>
2017-08-16 20:18:34 -0700

    sorry for general commit message, too much to say. Just uploading to move to my main PC

---
+++ menu.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Velo City Pizza</title>
+
+  <meta charset="utf-8" />
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="TEXT/CSS" href="css/grid.css">
+  <link href="https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="TEXT/CSS" href="css/style.css">
+
+
+  <body>
+
+    <header id=header>
+      <div class="background-texture">
+        <!-- BEGIN navbar.php INCLUDE -->
+        <?php include "navbar.php" ?>
+        <!-- END navbar.php INCLUDE -->
+        <img class="menu" src="img/placeholdermenu.jpg">
+    </header>
+
+
+
+  </body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Velo City Pizza</title>
+
+  <meta charset="utf-8" />
+  <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="TEXT/CSS" href="css/grid.css">
+  <link href="https://opensource.keycdn.com/fontawesome/4.6.3/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="TEXT/CSS" href="css/style.css">
+
+
+  <body>
+
+    <header id=header>
+      <div class="background-texture">
+        <!-- BEGIN test.php INCLUDE -->
+        <?php include "testing.php" ?>
+        <!-- END test.php INCLUDE -->
+    </header>
+
+
+
+  </body>
+
+</html>
